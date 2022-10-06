957ab8c6316ed5fe90d3413b85af7d30753170a1
maxwellbenton <maxwellbenton@gmail.com>
2022-10-06 06:59:36 -0400

    add allSettled removal button

--- otp-script.js
+++ otp-script.js
@@ -4,35 +4,40 @@ const state = {
 }
 
 async function init() {
-  const c2p = new window.Click2Pay({debug: true })
-  try {
-    console.log('cardBrands', state.cardBrands)
-    console.log(await c2p.init({
-      srcDpaId: 'b756a2b0-ef62-4c62-a6de-f72e75ce5f17',
-      dpaData: {
-        dpaName: 'SparkTmerch'
-      },
-      dpaLocale: 'en_US',
-      cardBrands: state.cardBrands
-    }))
+  const libJS = document.createElement('script')
+  libJS.src = 'https://sandbox.src.mastercard.com/srci/integration/2/lib.js'
+  await document.body.appendChild(libJS)
+  libJS.addEventListener('load', async function() {
+    const c2p = new window.Click2Pay({debug: true })
+    try {
+      console.log('cardBrands', state.cardBrands)
+      console.log(await c2p.init({
+        srcDpaId: 'b756a2b0-ef62-4c62-a6de-f72e75ce5f17',
+        dpaData: {
+          dpaName: 'SparkTmerch'
+        },
+        dpaLocale: 'en_US',
+        cardBrands: state.cardBrands
+      }))
 
-    console.log(await c2p.idLookup({ email: state.email }))
+      console.log(await c2p.idLookup({ email: state.email }))
 
-    try { await c2p.initiateValidation() } catch(e) { console.log(e)}
+      try { await c2p.initiateValidation() } catch(e) { console.log(e)}
 
-    const otpInput = document.createElement('src-otp-input')
-    await customElements.whenDefined('src-otp-input')
-    otpInput.cardBrands = state.cardBrands
-    otpInput.displayHeader = true
-    otpInput.type = "overlay"
-    otpInput.addEventListener('otpChanged', (event) => {
-      handleOTP(event, c2p, otpInput)
-    })
-    otpInput.addEventListener('input', console.log)
-    document.body.appendChild(otpInput)
-  } catch (e) {
-    console.log(e)
-  }
+      const otpInput = document.createElement('src-otp-input')
+      await customElements.whenDefined('src-otp-input')
+      otpInput.cardBrands = state.cardBrands
+      otpInput.displayHeader = true
+      otpInput.type = "overlay"
+      otpInput.addEventListener('otpChanged', (event) => {
+        handleOTP(event, c2p, otpInput)
+      })
+      otpInput.addEventListener('input', console.log)
+      document.body.appendChild(otpInput)
+    } catch (e) {
+      console.log(e)
+    }
+  })
 }
 
 function updateCardBrands(event) {
@@ -64,7 +69,11 @@ async function handleOTP(event, c2p, otpInput) {
   }
 }
 
-
+function removeAllSettled() {
+  console.log('Before removal', Promise.allSettled)
+  Promise.allSettled = undefined
+  console.log('After removal', Promise.allSettled)
+}
 
 (async function() {
   await customElements.whenDefined('src-mark')
@@ -75,6 +84,7 @@ async function handleOTP(event, c2p, otpInput) {
   emailInput = document.querySelector('#email')
   emailInput.value = state.email
   cardBrandsInput.addEventListener('input', updateCardBrands)
+  document.querySelector('button').addEventListener('click', removeAllSettled)
   mark.addEventListener('click', init)
 })()
 
